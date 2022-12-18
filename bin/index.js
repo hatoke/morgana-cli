7df1b849761a26c8c0b9ff11157cfd1f1749e7e5
Berkay UZUN <oluyum@gmail.com>
2022-12-19 02:48:43 +0300

    chore: add morgana mask ascii view

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,32 @@ const { Command } = require("commander");
 const { createApp, gitignoreFile } = require("./commands");
 
 const program = new Command();
-program.name("morgana-cli").description("Personal Cli").version("0.0.1", "-v, --vers", "output the current version");
+program
+  .name("morgana-cli")
+  .description("Personal Cli")
+  .version("0.0.1", "-v, --vers", "output the current version")
+  .action(() => {
+    console.log(`
+    Author Berkay Uzun
+                                                                                                      
+       @@@                               @@& .    
+      ,& @@@@@                       &@@@, @      
+       @    #@@@@@@@@@@@@@@@@@@@@@@@@@     % .    
+       @    @@@@@@@@@@@@@@@@@@@@@@@@@@@    @      
+        @@@@#.@@@@@@@@@@@@@@@@@@@@@@@ @@@&@       
+        @#       @@@@@@@@@@@@@@@@@       @@       
+       @#         @@@@@@@@@@/@@@@         @@ .    
+      @@          @@@@@@@@@@@@@@@         .@*     
+      @@.         @@@@@@@@@@@@@@@         @@@     
+      @@@        @@@@@@@@@@@@@@@@@       %@@(     
+     . @@@@(   @@@@@@@@@@@@@@@@@@@@@   @@@@@ .    
+      .     @@@@@@@@%@@@@@@@,@@@@@@@@@@     .     
+        .                                 .       
+                                                  
+               .                   .              
+                                                                                                    
+  `);
+  });
 
 program
   .command("create-app")
